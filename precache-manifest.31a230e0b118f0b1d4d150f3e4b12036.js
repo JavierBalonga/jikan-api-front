5e4c248b35c3ff0d36c2823f6852582c61f2d1e2
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-10-11 22:29:55 -0300

    Updates

---
+++ precache-manifest.31a230e0b118f0b1d4d150f3e4b12036.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359ae051cdf384a5c85cef56e408a61e",
+    "url": "/jikan-api-front/index.html"
+  },
+  {
+    "revision": "b81accc68225c60fd702",
+    "url": "/jikan-api-front/static/css/main.ece54db0.chunk.css"
+  },
+  {
+    "revision": "9ae7ef11fc8b2b533ac4",
+    "url": "/jikan-api-front/static/js/2.f11a8fae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jikan-api-front/static/js/2.f11a8fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81accc68225c60fd702",
+    "url": "/jikan-api-front/static/js/main.27b308f7.chunk.js"
+  },
+  {
+    "revision": "865e55d0923820ef1753",
+    "url": "/jikan-api-front/static/js/runtime-main.f3d885ce.js"
+  },
+  {
+    "revision": "1000f903e4c0bbe62f910caf786ab243",
+    "url": "/jikan-api-front/static/media/backgroundGradient.1000f903.png"
+  },
+  {
+    "revision": "4934e72d8b14fa58dbd98a342faf51cc",
+    "url": "/jikan-api-front/static/media/hexagon.4934e72d.svg"
+  }
+]);
